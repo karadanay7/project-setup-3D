ead0c3fb3bdcd6f119823f3c56fb1c3ee5d99f08
Aysegul <karadan.ay7@gmail.com>
2024-09-04 14:52:19 +0300

    updated

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <Navbar />
+  
     <NuxtPage />
-    <Footer />
+
   </div>
 </template>
--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="text-red-500">hello</div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
